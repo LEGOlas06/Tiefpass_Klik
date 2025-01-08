f10c546eeedf46bf93c1371b9d9ad38660ecfbcb
Leander Klik <160396774+LEGOlas06@users.noreply.github.com>
2025-01-08 12:09:05 +0100

    Das Ganze Projekt funktioniert

--- src/main.ino
+++ src/main.ino
@@ -7,46 +7,40 @@
 #include "lib/lcdarduino.h"
 #include <util/delay.h>
 
-/*
-Pin-Zuordnung:
-PB3 - MISO - in
-PB2 - MOSI - out
-PB1 - SCK - out
-PB0 - Chip Select (CS) Input - out
-PA0 - Chip Select (CS) Output - out
-PA1 - Latch Ausgang - out
-*/
+// Pin-Zuordnung
+#define CS_EINGANG_PIN PB0
+#define CS_AUSGANG_PIN PA0
+#define LATCH_PIN PA1
 
 // ADC-Daten
-volatile int adcHighByte = 0;
-volatile int adcLowByte = 0;
+volatile int adcOberesByte = 0;
+volatile int adcUnteresByte = 0;
 
 // Menü- und LCD-Steuerung
-volatile unsigned char menuStatus = 1;
-volatile unsigned char previousMenuStatus = 0;
-char displayBuffer[16];
-volatile char debounceFlag = 0;
-volatile char buttonLock = 0;
+volatile unsigned char menueStatus = 1;
+volatile unsigned char vorherigerMenueStatus = 0;
+char anzeigePuffer[16];
+volatile char entprellFlagge = 0;
+volatile char tastenSperre = 0;
 
 // Filter-Parameter
-volatile float previousOutput = 0;    // Vorheriger Filter-Ausgangswert
-volatile float currentOutput = 0;     // Aktueller Filter-Ausgangswert
-volatile float filterFrequency = 100;
-volatile float filterGain = 1;
-volatile float updatedFrequency = 100;
-volatile float updatedGain = 1;
+volatile float vorherigerAusgang = 0;
+volatile float aktuellerAusgang = 0;
+volatile float filterFrequenz = 100;
+volatile float filterVerstaerkung = 1;
+volatile float aktualisierteFrequenz = 100;
+volatile float aktualisierteVerstaerkung = 1;
 volatile float a0 = 0;
 volatile float b1 = 0;
 
 // SPI Interrupt Service Routine
-ISR(SPI_STC_vect) {
-}
+ISR(SPI_STC_vect) {}
 
 // Timer0 Compare Match A Interrupt für den Abtastzyklus
-ISR (TIMER0_COMPA_vect) {
+ISR(TIMER0_COMPA_vect) {
     // Start der SPI-Kommunikation mit dem ADC
-    PORTB &= ~(1 << PB0);  // Chip Select aktivieren
-    SPDR = 0b00000110;     // ADC-Datenanforderung senden
+    PORTB &= ~(1 << CS_EINGANG_PIN);
+    SPDR = 0b00000110;
     while (!(SPSR & (1 << SPIF)));
 
     // Weiteren Taktzyklus starten, um Daten zu empfangen
@@ -54,40 +48,36 @@ ISR (TIMER0_COMPA_vect) {
     while (!(SPSR & (1 << SPIF)));
 
     // Oberen 4 Bits des ADC-Wertes lesen
-    adcHighByte = (SPDR) & ~(0xF0);
-    SPDR = 0b00000000;  // Weitere Taktzyklen für die unteren Bits
+    adcOberesByte = (SPDR) & ~(0xF0);
+    SPDR = 0b00000000;
     while (!(SPSR & (1 << SPIF)));
 
     // Unteren 8 Bits des ADC-Wertes lesen
-    adcLowByte = SPDR;
-    PORTB |= (1 << PB0);  // Chip Select deaktivieren
+    adcUnteresByte = SPDR;
+    PORTB |= (1 << CS_EINGANG_PIN);
 
     // ADC-Daten zu einem Wert zusammenführen
-    float currentInput = (adcHighByte << 8) | adcLowByte;
+    float aktuellerEingang = (adcOberesByte << 8) | adcUnteresByte;
 
     // Filterberechnung für den Tiefpassfilter
-    currentOutput = (b1 * currentInput + a0 * previousOutput);
-
-    // Aktualisierung der vorherigen Ausgangswerte
-    previousOutput = currentOutput;
+    aktuellerAusgang = (b1 * aktuellerEingang + a0 * vorherigerAusgang);
+    vorherigerAusgang = aktuellerAusgang;
 
     // Berechnen und Begrenzen des Ausgangswertes
-    int outputSignal = updatedGain * (int)currentOutput - (updatedGain - 1) * 2048;
-
-    if (outputSignal <= 0) { outputSignal = 0; }
-    if (outputSignal >= 4095) { outputSignal = 4095; }
+    int ausgangsSignal = aktualisierteVerstaerkung * (int)aktuellerAusgang - (aktualisierteVerstaerkung - 1) * 2048;
+    ausgangsSignal = constrain(ausgangsSignal, 0, 4095);
 
     // Übertragung des Ausgangswertes zum DAC
-    PORTA &= ~(1 << PA0);  // DAC Chip Select aktivieren
-    SPDR = (0b00010000) | ((outputSignal >> 8) & 0x0F);  // Oberen 4 Bits senden
+    PORTA &= ~(1 << CS_AUSGANG_PIN);
+    SPDR = (0b00010000) | ((ausgangsSignal >> 8) & 0x0F);
     while (!(SPSR & (1 << SPIF)));
-    SPDR = outputSignal & 0xFF;  // Unteren 8 Bits senden
+    SPDR = ausgangsSignal & 0xFF;
     while (!(SPSR & (1 << SPIF)));
-    PORTA |= (1 << PA0);  // DAC Chip Select deaktivieren
+    PORTA |= (1 << CS_AUSGANG_PIN);
 
     // Latch-Ausgang setzen und zurücksetzen
-    PORTA &= ~(1 << PA1);
-    PORTA |= (1 << PA1);
+    PORTA &= ~(1 << LATCH_PIN);
+    PORTA |= (1 << LATCH_PIN);
 }
 
 void setup() {
@@ -100,79 +90,105 @@ void setup() {
     SPSR = 0;
 
     // Timer0-Konfiguration für 1 ms Abtastrate
-    TCCR0A = 2 << WGM00;  // CTC Modus
-    TCCR0B = 3 << CS00;   // Prescaler=64
-    OCR0A = 249;          // Zählerwert für 1 ms
-    TIMSK0 = 1 << OCIE0A; // Interrupt aktivieren
+    TCCR0A = 2 << WGM00;
+    TCCR0B = 3 << CS00;
+    OCR0A = 249;
+    TIMSK0 = 1 << OCIE0A;
 
     // Globale Interrupts aktivieren
     sei();
 
     // Chip Select auf Ausgang high setzen
-    PORTB |= (1 << PB0);
-    PORTA |= (1 << PA0);
-    PORTA |= (1 << PA1);
-    PORTB &= ~(1 << PB2); // MOSI initialisieren
+    PORTB |= (1 << CS_EINGANG_PIN);
+    PORTA |= (1 << CS_AUSGANG_PIN);
+    PORTA |= (1 << LATCH_PIN);
+    PORTB &= ~(1 << PB2);
 
     // LCD initialisieren und Startwerte anzeigen
     lcd_init(LCD_DISP_ON_CURSOR);
     lcd_clrscr();
-    sprintf(displayBuffer, "Frq: %d s-1", int(filterFrequency));
-    lcd_puts(displayBuffer);
-    sprintf(displayBuffer, "\nGain: %d", int(filterGain));
-    lcd_puts(displayBuffer);
+    sprintf(anzeigePuffer, "Frq: %d s-1", int(filterFrequenz));
+    lcd_puts(anzeigePuffer);
+    sprintf(anzeigePuffer, "\nGain: %d", int(filterVerstaerkung));
+    lcd_puts(anzeigePuffer);
+
+    // Initialisiere Filterparameter
+    aktualisierteFrequenz = filterFrequenz;
+    aktualisierteVerstaerkung = filterVerstaerkung;
+
+    // Berechnen der neuen Tiefpass-Koeffizienten
+    a0 = (1 / aktualisierteFrequenz) / (0.001 + 1 / aktualisierteFrequenz);
+    b1 = (0.001) / (0.001 + 1 / aktualisierteFrequenz);
+
+    vorherigerAusgang = aktuellerAusgang;
 }
 
 void loop() {
     lcd_home();
 
-    // Tastenabfrage zur Steuerung von Frequenz und Gain
-    char buttonPress = get_button();
-    switch (buttonPress) {
+    // Tastenabfrage zur Steuerung von Frequenz und Verstärkung
+    char tastenDruck = get_button();
+    switch (tastenDruck) {
         case button_up:
-            if (buttonLock == 0) {
-                filterGain++; buttonLock = 1; previousMenuStatus = 0;
-            } break;
+            if (tastenSperre == 0) {
+                filterVerstaerkung++;
+                tastenSperre = 1;
+                vorherigerMenueStatus = 0;
+            }
+            break;
         case button_down:
-            if (buttonLock == 0) {
-                filterGain--; buttonLock = 1; previousMenuStatus = 0;
-            } break;
+            if (tastenSperre == 0) {
+                filterVerstaerkung--;
+                tastenSperre = 1;
+                vorherigerMenueStatus = 0;
+            }
+            break;
         case button_left:
-            if (buttonLock == 0) {
-                filterFrequency--; buttonLock = 1; previousMenuStatus = 0;
-            } break;
+            if (tastenSperre == 0) {
+                filterFrequenz -= 10;
+                tastenSperre = 1;
+                vorherigerMenueStatus = 0;
+            }
+            break;
         case button_right:
-            if (buttonLock == 0) {
-                filterFrequency++; buttonLock = 1; previousMenuStatus = 0;
-            } break;
+            if (tastenSperre == 0) {
+                filterFrequenz += 10;
+                tastenSperre = 1;
+                vorherigerMenueStatus = 0;
+            }
+            break;
         case button_ok:
-            if (buttonLock == 0) {
-                buttonLock = 1; previousMenuStatus = 2;
-            } break;
-        default: buttonLock = 0; break;
+            if (tastenSperre == 0) {
+                tastenSperre = 1;
+                vorherigerMenueStatus = 2;
+            }
+            break;
+        default:
+            tastenSperre = 0;
+            break;
     }
 
     // LCD-Anzeige aktualisieren, wenn Menü geändert wurde
-    if (menuStatus != previousMenuStatus) {
-        if (previousMenuStatus == 0) {
-            sprintf(displayBuffer, "Frq: %d s-1", int(filterFrequency));
-            lcd_puts(displayBuffer);
-            sprintf(displayBuffer, "\nGain: %d", int(filterGain));
-            lcd_puts(displayBuffer);
-            previousMenuStatus = 1;
+    if (menueStatus != vorherigerMenueStatus) {
+        if (vorherigerMenueStatus == 0) {
+            sprintf(anzeigePuffer, "Frq.:%06d s-1", int(filterFrequenz));
+            lcd_puts(anzeigePuffer);
+            sprintf(anzeigePuffer, "\nVerst.:%04d", int(filterVerstaerkung));
+            lcd_puts(anzeigePuffer);
+            vorherigerMenueStatus = 1;
         }
 
         // Aktualisierte Filterparameter anwenden
-        if (previousMenuStatus == 2) {
-            updatedFrequency = filterFrequency;
-            updatedGain = filterGain;
+        if (vorherigerMenueStatus == 2) {
+            aktualisierteFrequenz = filterFrequenz;
+            aktualisierteVerstaerkung = filterVerstaerkung;
 
             // Berechnen der neuen Tiefpass-Koeffizienten
-            a0 = (1 / updatedFrequency) / (0.001 + 1 / updatedFrequency);
-            b1 = (0.001) / (0.001 + 1 / updatedFrequency);
+            a0 = (1 / aktualisierteFrequenz) / (0.001 + 1 / aktualisierteFrequenz);
+            b1 = (0.001) / (0.001 + 1 / aktualisierteFrequenz);
 
-            previousOutput = currentOutput;
-            previousMenuStatus = 1;
+            vorherigerAusgang = aktuellerAusgang;
+            vorherigerMenueStatus = 1;
         }
     }
 }
